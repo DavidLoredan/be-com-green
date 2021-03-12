ce2ff8fd71993b465956ceca1b5ca67fa1aa183a
thomassupervil <thomas.supervil@gmail.com>
2021-03-12 17:02:39 +0100

    add anim on scroll data -> js

--- script.js
+++ script.js
@@ -16,24 +16,54 @@ checkbox.addEventListener('change', function() {
 // counting up numbers
 
 const counters = document.querySelectorAll('.counter');
-const speed = 100;
+const nbStep = 100 ;
 
-counters.forEach(counter => {
-  const updateCount = () => {
-    const target = +counter.getAttribute('data-target')
-    const count = +counter.innerText;
+// counters.forEach(counter => {
+//   const updateCount = () => {
+//     const target = +counter.getAttribute('data-target')
+//     const count = +counter.innerText;
 
-    const increment = Math.floor(target / speed);
+//     const increment = Math.floor(target / speed);
 
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount,1) //reload every 1ms
-    }
-    else {
-      count.innerText = target;
-    }
-  }
-  updateCount();
-});
+//     if (count < target) {
+//       counter.innerText = count + increment;
+//       setTimeout(updateCount,1) //reload every 1ms
+//     }
+//     else {
+//       count.innerText = target;
+//     }
+//   }
+//   updateCount();
+// });
 
 //animate on scroll
+
+let animationHasPlayed = false;
+// const page = document.querySelector('body');
+
+
+window.addEventListener('scroll', function(){
+  if (window.scrollY < 450 && animationHasPlayed === false) {
+    return;
+  }
+    animationHasPlayed = true;
+    console.log("hello");
+
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-target')
+        const count = +counter.innerText;
+    
+        const increment = Math.floor(target / nbStep);
+    
+        if (count < target) {
+          counter.innerText = count + increment;
+          setTimeout(updateCount,50) //reload every 1ms
+        }
+        else {
+          count.innerText = target;
+        }
+      }
+      updateCount();
+    });
+});
