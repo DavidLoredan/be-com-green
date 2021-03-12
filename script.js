e2249fe962a2ebfa4a8030fda44796879eff7668
thomassupervil <thomas.supervil@gmail.com>
2021-03-12 15:47:09 +0100

    add animation for data in js

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+// Darkmode
+
 let checkbox = document.querySelector(".dmCheck");
 let body = document.querySelector("body");
 
 checkbox.addEventListener('change', function() {
+  
   if (this.checked) {
     body.classList.add("dark");
   } else {
     body.classList.remove("dark");
   }
 });
+
+
+// counting up numbers
+
+const counters = document.querySelectorAll('.counter');
+const speed = 100;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target')
+    const count = +counter.innerText;
+
+    const increment = Math.floor(target / speed);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount,1) //reload every 1ms
+    }
+    else {
+      count.innerText = target;
+    }
+  }
+  updateCount();
+});
+
+//animate on scroll
