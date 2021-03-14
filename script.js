f531a2706e5d6f140095a248871d94aaeaaee48f
thomassupervil <thomas.supervil@gmail.com>
2021-03-14 19:33:58 +0100

    Change some css style on my part

--- script.js
+++ script.js
@@ -13,41 +13,19 @@ checkbox.addEventListener('change', function() {
 });
 
 
-// counting up numbers
+//animate on scroll
+//counting up numbers
 
 const counters = document.querySelectorAll('.counter');
 const nbStep = 100 ;
 
-// counters.forEach(counter => {
-//   const updateCount = () => {
-//     const target = +counter.getAttribute('data-target')
-//     const count = +counter.innerText;
-
-//     const increment = Math.floor(target / speed);
-
-//     if (count < target) {
-//       counter.innerText = count + increment;
-//       setTimeout(updateCount,1) //reload every 1ms
-//     }
-//     else {
-//       count.innerText = target;
-//     }
-//   }
-//   updateCount();
-// });
-
-//animate on scroll
-
 let animationHasPlayed = false;
-// const page = document.querySelector('body');
-
 
 window.addEventListener('scroll', function(){
   if (window.scrollY < 450 && animationHasPlayed === false) {
     return;
   }
     animationHasPlayed = true;
-    console.log("hello");
 
     counters.forEach(counter => {
       const updateCount = () => {
